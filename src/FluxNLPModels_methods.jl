b386159d49fe5ff5c67f1f9f42a140cdee4ab2c0
tmigot <tangi.migot@gmail.com>
2023-07-12 13:43:02 -0400

    Update docstring in NLPModel API (#19)

--- src/FluxNLPModels_methods.jl
+++ src/FluxNLPModels_methods.jl
@@ -1,14 +1,15 @@
 """
-    f = obj(nlp, x)
+    f = obj(nlp, w)
 
-Evaluate `f(x)`, the objective function of `nlp` at `x`.
+Evaluate `f(w)`, the objective function of `nlp` at `w`.
 
 # Arguments
-- `nlp::AbstractFluxNLPModel{T, S}`: the FluxNLPModel data struct
-- `w::AbstractVector{T}`: is the vector of weights/variables;
+- `nlp::AbstractFluxNLPModel{T, S}`: the FluxNLPModel data struct;
+- `w::AbstractVector{T}`: is the vector of weights/variables.
 
 # Output
-- `f_w`: the new objective function
+- `f_w`: the new objective function.
+
 """
 function NLPModels.obj(nlp::AbstractFluxNLPModel{T, S}, w::AbstractVector{T}) where {T, S}
   increment!(nlp, :neval_obj)
@@ -18,16 +19,18 @@ function NLPModels.obj(nlp::AbstractFluxNLPModel{T, S}, w::AbstractVector{T}) wh
 end
 
 """
-    g = grad!(nlp, x, g)
+    g = grad!(nlp, w, g)
 
-Evaluate `∇f(x)`, the gradient of the objective function at `x` in place.
+Evaluate `∇f(w)`, the gradient of the objective function at `w` in place.
+
 # Arguments
-- `nlp::AbstractFluxNLPModel{T, S}`: the FluxNLPModel data struct
+- `nlp::AbstractFluxNLPModel{T, S}`: the FluxNLPModel data struct;
 - `w::AbstractVector{T}`: is the vector of weights/variables;
--`g::AbstractVector{T}`: the gradient vector
+- `g::AbstractVector{T}`: the gradient vector.
 
 # Output
-- `g`: the gradient at point x
+- `g`: the gradient at point `w`.
+
 """
 function NLPModels.grad!(
   nlp::AbstractFluxNLPModel{T, S},
@@ -42,17 +45,17 @@ function NLPModels.grad!(
 end
 
 """
-    objgrad!(nlp, x, g)
+    objgrad!(nlp, w, g)
 
-    Evaluate both `f(x)`, the objective function of `nlp` at `x` and `∇f(x)`, the gradient of the objective function at `x` in place.
+Evaluate both `f(w)`, the objective function of `nlp` at `w`, and `∇f(w)`, the gradient of the objective function at `w` in place.
 
 # Arguments
-- `nlp::AbstractFluxNLPModel{T, S}`: the FluxNLPModel data struct
+- `nlp::AbstractFluxNLPModel{T, S}`: the FluxNLPModel data struct;
 - `w::AbstractVector{T}`: is the vector of weights/variables;
--`g::AbstractVector{T}`: the gradient vector
+- `g::AbstractVector{T}`: the gradient vector.
 
 # Output
-- `f_w`, `g`: the new objective function, and the gradient at point x
+- `f_w`, `g`: the new objective function, and the gradient at point w.
 
 """
 function NLPModels.objgrad!(
@@ -61,7 +64,6 @@ function NLPModels.objgrad!(
   g::AbstractVector{T},
 ) where {T, S}
   @lencheck nlp.meta.nvar w g
-  #both updates
   increment!(nlp, :neval_obj)
   increment!(nlp, :neval_grad)
   set_vars!(nlp, w)
